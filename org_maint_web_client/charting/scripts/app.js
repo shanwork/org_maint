f7a658ff37fdda0cfb937547220902e264af44f8
shanwork <shanwork2012@gmail.com>
2016-01-19 06:26:37 -0800

    basic charting added

---
+++ org_maint_web_client/charting/scripts/app.js
@@ -0,0 +1,87 @@
+(function() {
+    
+   
+    var charting_app = angular.module('charting_app', ['ngRoute', 'ngSanitize', 'ngStorage']);
+    budgetApp.value('connectToService', 'false');
+    budgetApp.value('configuration', { imMemoryData: 'yes', verbose: 'yes' });
+    budgetApp.config(function ($routeProvider) {
+        $routeProvider
+          //.when('/entityFinanceSummaries', {
+          //    controller: 'EntityFinanceSummaryController',
+              
+          //      templateUrl: 'views/entityFinanceSummaries.html'
+          //  })
+          //   .when('/entityExpenses/:EntityFinanceSummaryID/:EntityName', {
+          //       controller: 'EntityItemExpensesController',
+          //       templateUrl: 'views/entityItemExpenses.html'
+          //   })
+          //  .when('/contributors', {
+          //      controller: 'ContributorController',
+          //      templateUrl: 'views/contributorList.html'
+          //  })
+          //  .when('/budgetStatus', {
+          //      controller: 'BudgetStatusController',
+          //      templateUrl: 'views/budgetStatus.html'
+          //  })
+          //   .when('/about', {
+          //       controller: 'AboutThisSiteController',
+          //       templateUrl: 'views/aboutThisSite.html'
+          //   })
+          //  .when('/home', {
+          //      templateUrl: '../../index.html'
+          //  })
+          //  .when('/settings', {
+          //      controller:'SettingsController',
+          //      templateUrl: 'views/settings.html'
+
+          //  }).when('/charting', {
+          //     // controller: 'SettingsController',
+          //      templateUrl: 'views/charting.html'
+
+          //  })
+            .otherwise({ redirectTo: '/budgetStatus' });
+    },function ($httpProvider) {
+        $httpProvider.defaults.headers.common = {};
+        $httpProvider.defaults.headers.post = {};
+        $httpProvider.defaults.headers.put = {};
+        $httpProvider.defaults.headers.patch = {};
+    });
+    budgetApp.directive('clickLink', ['$location', function ($location) {
+        return {
+            link: function (scope, element, attrs) {
+                element.on('click', function () {
+                    scope.$apply(function () {
+                        $location.path(attrs.clickLink);
+                    });
+                });
+            }
+        }
+    }]);
+    budgetApp.directive('clickLinkConfirm', ['$location', function ($location) {
+        return {
+            link: function (scope, element, attrs) {
+                element.on('click', function () {
+                    scope.$apply(function () {
+                        if (confirm("are you sure you want to return?"))
+                            $location.path(attrs.clickLinkConfirm);
+                    });
+                });
+            }
+        }
+    }]);
+    //budgetApp.config(function ($routeProvider) {
+    //    $routeProvider
+    //        .when('/contributors', {
+    //            controller: 'ContributorController',
+    //            templateUrl: 'views/contributorList.html'
+    //        })
+    //        .when('/budgetStatus', {
+    //            controller: 'BudgetStatusController',
+    //            templateUrl: 'views/budgetStatus.html'
+    //        })
+    //        .when('/home', {
+    //            templateUrl: '../../index.html'
+    //        })
+    //        .otherwise({ redirectTo: '/budgetStatus' });
+    //});
+}());
