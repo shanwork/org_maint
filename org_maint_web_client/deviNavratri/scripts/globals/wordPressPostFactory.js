c52ed5de750aeb8e95f74767ef3770a67b41cf5b
shanwork <shanwork2012@gmail.com>
2020-10-28 20:31:15 -0700

    Durga drawings

---
+++ org_maint_web_client/deviNavratri/scripts/globals/wordPressPostFactory.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+    angular.module('test_app').factory("WordPressPostFactory", ['$http', '$q',
+          function ($http, $q) {
+
+              var wordPressPostFactory = {};
+              wordPressPostFactory.getWordPressPosts = function ( callBack) {
+                 var url = 'https://public-api.wordpress.com/rest/v1/sites/idcdistro.wordpress.com/posts';
+                  $http.get(url).
+                    success(
+                      function (data) {
+                          console.log(data);
+                          callBack(data);
+                      }
+                  ).
+                  error(
+                     function () {
+                         callBack(null);
+                     }
+                  )
+              }
+             
+              return wordPressPostFactory;
+
+          } ]);
+})();
